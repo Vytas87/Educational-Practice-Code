5d5e425f6ccbd7f8857b457ad00fbca2078ab4b1
Vytautas Zarauskas <v.zarauskas@gmail.com>
2020-03-03 22:35:11 +0100

    Create 1. Trip Planner.java

---
+++ edX_Java Course_Learn to Program in Java/1. Trip Planner.java
@@ -0,0 +1,10 @@
+/**********************************************************************************************
+    This is the first project in the course. It tests the ability to use the very fundamental
+    concepts of the introduction to Java: structuring code into methods that carry out specific
+    tasks according to SOLID principles, variable declaration, implementing interacive code,
+    etc.
+    
+    The task is to create an interactive program that asks the user various practical questions
+    about the planned trip and gives answers.
+***********************************************************************************************/
+
