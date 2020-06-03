be6c27fa3214eb7639db41a7364235697fd04ac9
Vytautas Zarauskas <v.zarauskas@gmail.com>
2020-06-03 10:58:26 +0200

    Create 1.12 FightSong.java

---
+++ Practice-It/Chapter 1 - Introduction to Java Programming/1.12 FightSong.java
@@ -0,0 +1,76 @@
+/**********************************************************************************************
+Write a complete Java program in a class named FightSong that generates the following three
+figures of output. Use static methods to show structure and eliminate redundancy in your
+solution.
+
+In particular, make sure that main contains no System.out.println statements, that any
+System.out.println statement(s) repeated are captured in a method that is called just once,
+and that the same System.out.println statement never appears in two places in the code.
+
+                    Go, team, go!
+                    You can do it.
+
+                    Go, team, go!
+                    You can do it.
+                    You're the best,
+                    In the West.
+                    Go, team, go!
+                    You can do it.
+
+                    Go, team, go!
+                    You can do it.
+                    You're the best,
+                    In the West.
+                    Go, team, go!
+                    You can do it.
+
+                    Go, team, go!
+                    You can do it.
+
+***********************************************************************************************/
+
+public class FightSong {
+    public static void main(String[] args) {
+        goYouLineGoYou();
+        bestWest();
+        goYouLineGoYou();
+        bestWest();
+        goYouLineGoYou();
+    }
+    
+    public static void goYouLineGoYou() {
+        goYou();
+        line();
+        goYou();
+    }
+    
+    public static void goYou() {
+        go();
+        you();
+    }
+    
+    public static void bestWest() {
+        best();
+        west();
+    }
+    
+    public static void go() {
+        System.out.println("Go, team, go!");
+    }
+    
+    public static void you() {
+        System.out.println("You can do it.");
+    }
+    
+    public static void best() {
+        System.out.println("You're the best,");
+    }
+    
+    public static void west() {
+        System.out.println("In the West.");
+    }
+    
+    public static void line() {
+        System.out.println("");
+    }
+}
