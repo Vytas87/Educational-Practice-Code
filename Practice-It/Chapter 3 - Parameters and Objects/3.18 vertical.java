f42d0c985bec26745c1c3105e5ccea1c467da6dd
Vytautas Zarauskas <v.zarauskas@gmail.com>
2020-06-05 16:02:36 +0200

    Create 3.18 vertical.java

---
+++ Practice-It/Chapter 3 - Parameters and Objects/3.18 vertical.java
@@ -0,0 +1,20 @@
+/**********************************************************************************************
+Write a method called vertical that accepts a String as its parameter and prints each letter of
+the string on separate lines. For example, a call of vertical("hey now") should produce the
+following output:
+
+                    h
+                    e
+                    y
+
+                    n
+                    o
+                    w
+
+***********************************************************************************************/
+
+public static void vertical(String s) {
+    for (int i = 0; i < s.length(); i++) {
+        System.out.println(s.charAt(i));
+    }
+}
