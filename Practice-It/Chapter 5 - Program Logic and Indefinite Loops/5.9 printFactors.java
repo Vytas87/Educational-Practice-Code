741ee230d9e804a0f75707e7ea14b97380f8375d
Vytautas Zarauskas <v.zarauskas@gmail.com>
2020-06-09 09:33:07 +0200

    Create 5.9 printFactors.java

---
+++ Practice-It/Chapter 5 - Program Logic and Indefinite Loops/5.9 printFactors.java
@@ -0,0 +1,17 @@
+/**********************************************************************************************
+Write a method named printFactors that accepts an integer as its parameter and uses a fencepost
+loop to print the factors of that number, separated by the word " and ". For example, the number 24's factors should print as:
+
+                    1 and 2 and 3 and 4 and 6 and 8 and 12 and 24
+
+You may assume that the number parameter's value is greater than 0.
+***********************************************************************************************/
+
+public static void printFactors(int n) {
+    for (int i = 1; i <= n / 2; i++) {
+        if (n % i == 0) {
+            System.out.print(i + " and ");
+        }
+    }
+    System.out.println(n);
+}
