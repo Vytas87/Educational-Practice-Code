f944ddb1b3b13da5fa88e0f45bf1b78f7fad6f89
Vytautas Zarauskas <v.zarauskas@gmail.com>
2020-06-21 13:47:08 +0200

    Create Car.java

---
+++ Practice-It/Chapter 9 - Inheritance and Interfaces/9.4 Car/Car.java
@@ -0,0 +1,27 @@
+public class Car {
+	public void m1() {
+		System.out.println("car 1");
+	}
+
+	public void m2() {
+		System.out.println("car 2");
+	}
+
+	public String toString() {
+		return "vroom";
+	}
+}
+
+public class Truck extends Car {
+	public void m1() {
+		System.out.println("truck 1");
+	}
+
+	public void m2() {
+		super.m1();
+	}
+
+	public String toString() {
+		return super.toString() + super.toString();
+	}
+}
