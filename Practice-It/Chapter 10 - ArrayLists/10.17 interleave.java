7063149157b9b320e2eda909cfc09bbf0ccb7a10
Vytautas Zarauskas <v.zarauskas@gmail.com>
2020-06-24 09:38:25 +0200

    Create 10.17 interleave.java

---
+++ Practice-It/Chapter 10 - ArrayLists/10.17 interleave.java
@@ -0,0 +1,22 @@
+/**********************************************************************************************
+Write a method called interleave that accepts two ArrayLists of integers a1 and a2 as parameters
+and inserts the elements of a2 into a1 at alternating indexes. If the lists are of unequal
+length, the remaining elements of the longer list are left at the end of a1. For example, if a1
+stores [10, 20, 30] and a2 stores [4, 5, 6, 7, 8], the call of interleave(a1, a2); should change
+a1 to store [10, 4, 20, 5, 30, 6, 7, 8]. If a1 had stored [10, 20, 30, 40, 50] and a2 had stored
+[6, 7, 8], the call of interleave(a1, a2); would change a1 to store [10, 6, 20, 7, 30, 8, 40, 50].
+***********************************************************************************************/
+
+public static void interleave(ArrayList<Integer> a1, ArrayList<Integer> a2) {
+    int size = Math.min(a1.size(), a2.size());
+    
+    for (int i = 0; i < size; i++) {
+        a1.add(2 * i + 1, a2.get(i));
+    }
+    
+    if (a2.size() > size) {
+        for (int i = size; i < a2.size(); i++) {
+            a1.add(a2.get(i));
+        }
+    }
+}
