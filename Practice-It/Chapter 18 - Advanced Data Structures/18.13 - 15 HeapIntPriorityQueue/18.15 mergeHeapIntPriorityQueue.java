665ebeb295e6f87618fb363851b97c2fed7dd000
Vytautas Zarauskas <v.zarauskas@gmail.com>
2020-09-08 11:46:08 +0200

    Create 18.15 mergeHeapIntPriorityQueue.java

---
+++ Practice-It/Chapter 18 - Advanced Data Structures/18.13 - 15 HeapIntPriorityQueue/18.15 mergeHeapIntPriorityQueue.java
@@ -0,0 +1,13 @@
+/**********************************************************************************************
+Write a method in the HeapIntPriorityQueue class called merge that accepts another
+HeapIntPriorityQueue as a parameter and adds all elements from the other queue into the current
+queue, maintaining proper heap order such that the elements will still come out in ascending
+order when they are removed. Your code should not modify the queue passed in as a parameter.
+(Recall that objects of the same class can access each other's private fields.)
+***********************************************************************************************/
+
+public void merge(HeapIntPriorityQueue other) {
+    for (int i = 1; i <= other.size; i++) {
+        add(other.elementData[i]);
+    }
+}
